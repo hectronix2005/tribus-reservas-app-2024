0d7e55ebdfe07c6b85d918d72afbc5e30737b8b6
Hector Neira <hectorneira@Hectors-MacBook-Air.local>
2025-08-18 19:30:17 -0500

    Simplify server.js and clean dependencies

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, function() {
   console.log(`Server is running on port ${PORT}`);
 });
